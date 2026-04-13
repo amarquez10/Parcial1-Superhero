960656b90ee01928924cbce8a000d81a5c1bb4f2
Andrés <amarquez259@hotmail.com>
2026-04-12 22:34:13 -0500

    Commit inicial

---
+++ app/src/main/java/com/example/parcial1movilsuperheroe/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.parcial1movilsuperheroe
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.parcial1movilsuperheroe.ui.theme.Parcial1MovilSuperHeroeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Parcial1MovilSuperHeroeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Parcial1MovilSuperHeroeTheme {
+        Greeting("Android")
+    }
+}
